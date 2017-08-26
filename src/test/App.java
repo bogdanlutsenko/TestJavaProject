0f417969230faa56b0dc9607ce35845b4368cca1
bogdanlutsenko <1danya0443@gmail.com>
2017-08-26 13:10:56 -0400

    multi catch added and changes to App file

---
+++ src/test/App.java
@@ -0,0 +1,10 @@
+package test;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub ! ! !");
+		System.out.println("Hello again");
+	}
+
+}
---
+++ src/test/MultipleCatch.java
@@ -0,0 +1,27 @@
+package test;
+
+public class MultipleCatch {
+	public static void main(String[] args) {
+
+		
+		int[] nums={43,2,154,23};
+		try{
+		System.out.println(nums[1]/0);
+		}catch(NullPointerException e){
+			System.out.println("Catch block");
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("Catch block due to index");
+		}catch(ArithmeticException e){
+			System.out.println("Catch block due to /0");
+		}catch(Exception e){
+			System.out.println("Catch block due to unnown");
+		}
+		
+		System.out.println("After TRY Catch");
+		
+		
+		
+		
+		
+	}
+}
